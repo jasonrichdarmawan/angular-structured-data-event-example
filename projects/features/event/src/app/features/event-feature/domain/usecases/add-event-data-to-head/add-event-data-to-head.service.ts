e54d3f056472d1e3665791dd92819999222083db
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-12-15 17:44:29 +0700

    fix: add clause to prevent server function called in browser

--- projects/features/event/src/app/features/event-feature/domain/usecases/add-event-data-to-head/add-event-data-to-head.service.ts
+++ projects/features/event/src/app/features/event-feature/domain/usecases/add-event-data-to-head/add-event-data-to-head.service.ts
@@ -1,13 +1,21 @@
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { EventData } from '../../entities/event-data';
-import { DOCUMENT } from '@angular/common';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 @Injectable()
 export class AddEventDataToHeadService {
+  private isBrowser: boolean;
 
-  constructor(@Inject(DOCUMENT) private document: Document) { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) platformID: Object,
+  ) {
+    this.isBrowser = isPlatformBrowser(platformID);
+  }
 
   call(eventData: EventData) {
+    if (this.isBrowser) { return }
+
     let element = this.document.createElement('script');
     element.type = 'application/ld+json';
     element.text = JSON.stringify(eventData);
